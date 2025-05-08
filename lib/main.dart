ceb48c051ea8e98a18890022f964aac4357fe9fc
blalfayed <fayedblal@yahoo.com>
2025-05-08 18:14:19 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:ecommerce_supabase/views/auth/ui/login_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const OurMarket());
+}
+
+class OurMarket extends StatelessWidget {
+  const OurMarket({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Our Market',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginView(),
+    );
+  }
+}
