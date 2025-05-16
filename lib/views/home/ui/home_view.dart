1d1113066b603af3b5e8cd8df1aa07ecb2eaa6c2
blalfayed <fayedblal@yahoo.com>
2025-05-16 19:33:45 +0300

    create product cart and add image asset to it

--- lib/views/home/ui/home_view.dart
+++ lib/views/home/ui/home_view.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:ecommerce_supabase/core/app_colors.dart';
 import 'package:flutter/material.dart';
 
 import '../../../core/components/custom_Search_field.dart';
+import 'widgets/categories_list.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -25,64 +25,31 @@ class HomeView extends StatelessWidget {
             "مواد العطارة الشعبية",
             style: TextStyle(fontSize: 20),
           ),
-          const CategoriesList()
+          const CategoriesList(),
+          const SizedBox(
+            height: 20,
+          ),
+          Card(
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16))),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(16),
+                          bottomLeft: Radius.circular(16),
+                          bottomRight: Radius.circular(16)),
+                      child: Image.asset("assets/images/tawabel.jpg"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
   }
-}
-
-class CategoriesList extends StatelessWidget {
-  const CategoriesList({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: categories.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundColor: AppColors.kPrimaryColor,
-                    foregroundColor: AppColors.kWhiteColor,
-                    child: categories[index].icon,
-                  ),
-                  Text(
-                    categories[index].text,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 17),
-                  )
-                ],
-              ),
-            );
-          }),
-    );
-  }
-}
-
-List<Category> categories = [
-  // Category(icon: Icons.sports, text: 'Sports'),
-  // Category(icon: Icons.tv, text: 'Electronics'),
-  // Category(icon: Icons.collections, text: 'Collections'),
-  // Category(icon: Icons.book, text: 'books'),
-  // Category(icon: Icons.games, text: 'Games'),
-  Category(icon: Image.asset('assets/icons/spices.png'), text: 'التوابل'),
-  Category(icon: Image.asset('assets/icons/herbs.png'), text: 'الأعشاب'),
-];
-
-class Category {
-  final Widget icon;
-  final String text;
-  Category({
-    required this.icon,
-    required this.text,
-  });
 }
