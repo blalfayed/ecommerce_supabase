6fa0c867ac1a9b077bc86fa46c358f29a7c4290f
blalfayed <fayedblal@yahoo.com>
2025-05-26 01:53:12 +0300

    finish profile view ui

--- lib/views/profile/ui/profile_view.dart
+++ lib/views/profile/ui/profile_view.dart
@@ -1,56 +1,45 @@
 import 'package:ecommerce_supabase/core/app_colors.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_row_btn.dart';
+
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: AppColors.kWhiteColor,
-      margin: const EdgeInsets.all(16),
-      shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(24))),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            CircleAvatar(
-              radius: 55,
-              backgroundColor: AppColors.kPrimaryColor,
-              foregroundColor: AppColors.kWhiteColor,
-              child: Icon(
-                Icons.person,
-                size: 60,
-              ),
-            ),
-            Text('User Name'),
-            Text('User Email'),
-            Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(16))),
-              color: AppColors.kPrimaryColor,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(
-                      Icons.person,
-                      color: AppColors.kWhiteColor,
-                    ),
-                    Text(
-                      'Edit Name',
-                      style: TextStyle(
-                          color: AppColors.kWhiteColor,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Icon(Icons.arrow_forward_ios, color: AppColors.kWhiteColor)
-                  ],
+    return Center(
+      child: SizedBox(
+        height: MediaQuery.sizeOf(context).height * .65,
+        child: const Card(
+          color: AppColors.kWhiteColor,
+          margin: EdgeInsets.all(16),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(24))),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 55,
+                  backgroundColor: AppColors.kPrimaryColor,
+                  foregroundColor: AppColors.kWhiteColor,
+                  child: Icon(
+                    Icons.person,
+                    size: 60,
+                  ),
                 ),
-              ),
-            )
-          ],
+                Text('User Name'),
+                Text('User Email'),
+                CustomRowBtn(
+                  text: 'Edit Email',
+                  icon: Icons.person,
+                ),
+                CustomRowBtn(text: 'My Orders', icon: Icons.shopping_basket),
+                CustomRowBtn(text: 'Log Out', icon: Icons.logout)
+              ],
+            ),
+          ),
         ),
       ),
     );
