045c9bda0118575ce87f5f5de7b06efac265d372
blalfayed <fayedblal@yahoo.com>
2025-05-26 22:14:44 +0300

    start auth and add login fuction

---
+++ lib/views/product_details/logic/cubit/authentication_cubit.dart
@@ -0,0 +1,28 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+part 'authentication_state.dart';
+
+class AuthenticationCubit extends Cubit<AuthenticationState> {
+  AuthenticationCubit() : super(AuthenticationInitial());
+
+  SupabaseClient client = Supabase.instance.client;
+
+  Future<void> login({required String email, required String password}) async {
+    emit(LoginLoading());
+    try {
+      await client.auth.signInWithPassword(email: email, password: password);
+      emit(LoginSuccess());
+    } on AuthException catch (e) {
+      log(e.toString());
+      emit(LoginError(e.message));
+    } catch (e) {
+      log(e.toString());
+
+      emit(LoginError(e.toString()));
+    }
+  }
+}
