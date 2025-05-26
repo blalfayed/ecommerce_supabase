5b90fe9dab408f93a9908ba2fe90ffbdd477acef
blalfayed <fayedblal@yahoo.com>
2025-05-26 17:44:12 +0300

    add rating bar for product details view

--- lib/views/product_details/ui/product_details_view.dart
+++ lib/views/product_details/ui/product_details_view.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_supabase/core/components/cache_image.dart';
 import 'package:ecommerce_supabase/core/functions/build_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class ProductDetailsView extends StatelessWidget {
   const ProductDetailsView({super.key});
@@ -10,10 +11,64 @@ class ProductDetailsView extends StatelessWidget {
     return Scaffold(
       appBar: buildCustomAppBar(context, 'Product Name'),
       body: ListView(
-        children: const [
-          CachImage(
+        children: [
+          const CachImage(
             url:
                 "https://sharkiatoday.com/wp-content/uploads/2016/08/htqg-5-jhfg-hpvwd-ugd-ih-td-lfp.jpg",
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 16),
+            child: Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text('Product Name'), Text('123 LE')],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text('3  '),
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        )
+                      ],
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.grey,
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text('Product Description'),
+                const SizedBox(
+                  height: 20,
+                ),
+                RatingBar.builder(
+                  initialRating: 3,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  itemBuilder: (context, _) => const Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  },
+                )
+              ],
+            ),
           )
         ],
       ),
