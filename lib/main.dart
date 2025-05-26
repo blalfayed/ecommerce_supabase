6232f761cfcb259ec154c82676da40f7c76cabfc
blalfayed <fayedblal@yahoo.com>
2025-05-26 21:36:11 +0300

    initialize supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:ecommerce_supabase/core/app_colors.dart';
 import 'package:ecommerce_supabase/views/nav_bar/ui/main_home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://xprtclqdfccliascldhz.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwcnRjbHFkZmNjbGlhc2NsZGh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyODMzMzYsImV4cCI6MjA2Mzg1OTMzNn0.jojocl-UA37S2GPKvY-AUusaGBgUf-ItF1oVJpZ5IdM',
+  );
+
   runApp(const OurMarket());
 }
 
