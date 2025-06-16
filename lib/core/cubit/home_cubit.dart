bf097c7e73f7a516c46fc5ac64103180f6336ead
blalfayed <fayedblal@yahoo.com>
2025-06-16 05:24:41 +0300

    search logic part 1

--- lib/core/cubit/home_cubit.dart
+++ lib/core/cubit/home_cubit.dart
@@ -14,7 +14,9 @@ class HomeCubit extends Cubit<HomeState> {
   final ApiServices _apiServices = ApiServices();
   List<ProductModel> products = [];
 
-  Future<void> getProducts() async {
+  List<ProductModel> searchResults = [];
+
+  Future<void> getProducts({String? query}) async {
     emit(GetDataLoading());
 
     try {
@@ -23,10 +25,21 @@ class HomeCubit extends Cubit<HomeState> {
       for (var product in response.data as List) {
         products.add(ProductModel.fromJson(product));
       }
+      search(query);
       emit(GetDataSuccess());
     } catch (e) {
       log(e.toString());
       emit(GetDataError());
+    }
+  }
+
+  void search(String? query) {
+    if (query != null) {
+      for (var product in products) {
+        if (product.productName!.toLowerCase().contains(query.toLowerCase())) {
+          searchResults.add(product);
+        }
+      }
     }
   }
 }
